ba90900253e761dafd12b29810ad1e7a3dfe65bc
Joonsung Lee <joonsung@devsisters.com>
2016-01-09 06:27:40 +0900

    Add "-framework Security" on build flag

--- goquic.go
+++ goquic.go
@@ -2,7 +2,7 @@ package goquic
 
 // #cgo CXXFLAGS: -DUSE_OPENSSL=1 -std=gnu++11
 // #cgo LDFLAGS: -pthread -lgoquic -lquic -lssl -lcrypto -lstdc++ -lm -lprotobuf
-// #cgo darwin LDFLAGS: -framework CoreFoundation -framework Cocoa
+// #cgo darwin LDFLAGS: -framework CoreFoundation -framework Cocoa -framework Security
 // #include <stddef.h>
 // #include "src/adaptor.h"
 import "C"
